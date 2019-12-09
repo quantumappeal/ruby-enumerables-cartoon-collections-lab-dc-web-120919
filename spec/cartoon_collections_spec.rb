83d6ffcd6f181cc246ace60d9b7c8e5a6e9c069d
Sean Beach <sean.beach1@yahoo.com>
2019-12-09 03:22:44 +0000

    Automatically backed up by Learn

--- spec/cartoon_collections_spec.rb
+++ spec/cartoon_collections_spec.rb
@@ -80,3 +80,11 @@ end
 def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |call| call.length > 4 }
 end
+
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w[gouda cheddar camembert]
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
+end
