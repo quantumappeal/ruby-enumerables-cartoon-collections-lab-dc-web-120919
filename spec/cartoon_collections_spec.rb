4c63266b4883538104a6227539973b50f2d09808
Sean Beach <sean.beach1@yahoo.com>
2019-12-09 03:22:57 +0000

    Done.

--- spec/cartoon_collections_spec.rb
+++ spec/cartoon_collections_spec.rb
@@ -66,3 +66,25 @@ describe "Cartoon Collections" do
     end
   end
 end
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
+end
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize + '!' }
+end
+
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? { |call| call.length > 4 }
+end
+
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w[gouda cheddar camembert]
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
+end
